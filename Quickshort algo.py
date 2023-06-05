219e8424b68fe584a0f5aad945621fa1700522bd
Thibaut Busschaert <tbusschaertlille@gmail.com>
2023-06-05 16:55:54 +0200

    docs: python exercises

--- Quickshort algo.py
+++ Quickshort algo.py
@@ -2,20 +2,47 @@ sublist_left = []
 sublist_right = []
 final_list = []
 
-def partitionner(list):
+def quicksort(list):
     dernier = len(list)
     pivot = list[dernier -1]
     final_list.append(pivot)
     for i in range(dernier):
-        if pivot > list[i]:
+        if pivot >= list[i]:z 
             sublist_left.append(list [i])
-        elif pivot < list[i]:
+        else:
             sublist_right.insert(0,list[i])
-    if len(sublist_left) > 1:
-        partitionner(sublist_left)
+    if len(sublist_left) < 1:
+        quicksort(sublist_left)
     if len(sublist_right) > 1:
-         partitionner(sublist_right)
-    print(str(sublist_left)+str(final_list)+str(sublist_right))
-    return
+         quicksort(sublist_right)
+    return quicksort(sublist_left) + [final_list] + quicksort(sublist_right)
 
-partitionner ([4, 9, 3, 5, 1, 8, 7, 2, 6])
+print(quicksort([85,42,12,6,3,9,4,7,50]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# list = [15,56,2,9,4,23,19]
+
+# def random_pivot(list):
+#     index_pivot = random.choice(list)
+#     pivot = list(index_pivot)
+
+#     for i in range(len(list)):
+#         if i == index_pivot: continue
+
+#     print(pivot)
+#     return random_pivot
+
