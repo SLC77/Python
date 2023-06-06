bac0a3b25f386d07d2e8265016d4bfdd8e28975b
Thibaut Busschaert <tbusschaertlille@gmail.com>
2023-06-06 14:14:17 +0200

    Docs: Python exercices

--- Quickshort algo.py
+++ Quickshort algo.py
@@ -1,23 +1,38 @@
-sublist_left = []
-sublist_right = []
-final_list = []
+# sublist_left = []
+# sublist_right = []
+# final_list = []
 
-def quicksort(list):
-    dernier = len(list)
-    pivot = list[dernier -1]
-    final_list.append(pivot)
-    for i in range(dernier):
-        if pivot >= list[i]:z 
-            sublist_left.append(list [i])
-        else:
-            sublist_right.insert(0,list[i])
-    if len(sublist_left) < 1:
-        quicksort(sublist_left)
-    if len(sublist_right) > 1:
-         quicksort(sublist_right)
-    return quicksort(sublist_left) + [final_list] + quicksort(sublist_right)
+# def quicksort(list):
+#     dernier = len(list)
+#     pivot = list[dernier -1]
+#     final_list.append(pivot)
+#     for i in range(dernier):
+#         if pivot >= list[i]:z 
+#             sublist_left.append(list [i])
+#         else:
+#             sublist_right.insert(0,list[i])
+#     if len(sublist_left) < 1:
+#         quicksort(sublist_left)
+#     if len(sublist_right) > 1:
+#          quicksort(sublist_right)
+#     return quicksort(sublist_left) + [final_list] + quicksort(sublist_right)
 
-print(quicksort([85,42,12,6,3,9,4,7,50]))
+# print(quicksort([85,42,12,6,3,9,4,7,50]))
+
+
+def quicksort(liste:list) -> list:
+    if len(liste) <= 1:
+        return liste
+    
+    pivot_index = len(liste) - 1
+    pivot = liste[pivot_index]
+    
+    liste_gauche = [elt for elt in liste[:pivot_index] if elt <= pivot]
+    liste_droite = [elt for elt in liste[:pivot_index] if elt > pivot]
+    
+    return quicksort(liste_gauche) + [pivot] + quicksort(liste_droite) 
+
+print(quicksort([0, 0, 1, 15, 20, -4, 2, 1, 7, 18, 7]))
 
 
 
